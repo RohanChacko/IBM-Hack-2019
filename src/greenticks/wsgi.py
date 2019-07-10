99064516c98642c3d1dc85d60567e72fdaa872a1
AadilMehdi J Sanchawala <aadilmehdi@AadilMehdis-MacBook-Pro.local>
2019-07-10 10:35:03 +0400

    Adding the deployment settings into the wsgi IBM Cloud

--- src/greenticks/wsgi.py
+++ src/greenticks/wsgi.py
@@ -7,10 +7,26 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
+
+
+# Previous Settings
+# -----------------
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'greenticks.settings')
+
+# application = get_wsgi_application()
+
+
+# Deployment Settings
+# -------------------
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'greenticks.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "greenticks.settings")
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
