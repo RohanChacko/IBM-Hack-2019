cc3251eeed71e1c926270b74238ec29a1e730a82
AadilMehdi J Sanchawala <aadilmehdi@AadilMehdis-MacBook-Pro.local>
2019-07-11 16:55:25 +0400

    Linking the login to the button on index and changing to local settings

--- energy-auditor/greenticks/wsgi.py
+++ energy-auditor/greenticks/wsgi.py
@@ -11,22 +11,22 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 
 # Previous Settings
 # -----------------
-# import os
-#
-# from django.core.wsgi import get_wsgi_application
-#
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'greenticks.settings')
-#
-# application = get_wsgi_application()
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'greenticks.settings')
+
+application = get_wsgi_application()
 
 
 # Deployment Settings
 # -------------------
-import os
+# import os
 
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+# from django.core.wsgi import get_wsgi_application
+# from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "greenticks.settings")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "greenticks.settings")
 
-application = Cling(get_wsgi_application())
+# application = Cling(get_wsgi_application())
